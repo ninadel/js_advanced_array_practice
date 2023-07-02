c75311e09b2c910cd88927f68c8015555b17b7cc
Nina del Rosario <nina.del@gmail.com>
2023-07-02 12:21:40 -0700

    finished hasOddNumber function

--- js-array-some-every/some-every.js
+++ js-array-some-every/some-every.js
@@ -6,7 +6,11 @@ Examples:
     hasOddNumber([2,2,2,2,2,4]) // false
 */
 
-function hasOddNumber(arr) {}
+function hasOddNumber(arr) {
+  return arr.some(function (value) {
+    return value % 2 === 1;
+  });
+}
 
 /*
 Write a function called hasAZero which accepts a number and returns true if that number contains at least one zero. Otherwise, the function should return false
