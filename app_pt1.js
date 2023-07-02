85bc031853650542bbe6bdb05b8d4078b5e181ff
Nina del Rosario <nina.del@gmail.com>
2023-07-02 11:38:16 -0700

    renaming files

--- app_pt1.js
+++ app_pt1.js
@@ -273,3 +273,34 @@ function filterByValue(arr, test) {
 }
 
 console.log(filterByValue(filterByValueArray, filterByValueTest));
+
+// find
+// Write a function called find which accepts an array and a value and returns the first element in the array that has the same value as the second parameter or undefined if the value is not found in the array.
+// find([1,2,3,4,5], 3) // 3
+// find([1,2,3,4,5], 10) // undefined
+
+// findInObj
+// Write a function called findInObj which accepts an array of objects, a key, and some value to search for and returns the first found value in the array.
+// findInObj(
+//     [
+//       {first: 'Elie', last:"Schoppik"},
+//       {first: 'Tim', last:"Garcia", isCatOwner: true},
+//       {first: 'att', last:"Lane"},
+//       {first: 'Colt', last:"Steele", isCatOwner: true}
+//     ],
+//     'isCatOwner',
+//     true
+//   )
+
+//   // {first: 'Tim', last:"Garcia", isCatOwner: true}
+
+// removeVowels
+// Write a function called removeVowels which accepts a string and returns a new string with all of the vowels(both uppercased and lowercased) removed.Every character in the new string should be lowercased.
+// removeVowels('Elie') // ('l')
+// removeVowels('TIM') // ('tm')
+// removeVowels('ZZZZZZ') // ('zzzzzz')
+
+// doubleOddNumbers
+// Write a function called doubleOddNumbers which accepts an array and returns a new array with all of the odd numbers doubled(HINT - you can use map and fitler to double and then filter the odd numbers).
+// doubleOddNumbers([1,2,3,4,5]) // [2,6,10]
+// doubleOddNumbers([4,4,4,4,4]) // []
